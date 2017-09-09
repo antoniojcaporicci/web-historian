41a3e228213c6e6ebca30f0872dca259f73de859
Hack Reactor Student <student@hackreactor.com>
2017-09-08 20:00:02 -0500

    working on last test

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
+var http = require('http');
 
 /*
  * You will need to reuse the same paths many times over in the course of this sprint.
@@ -25,17 +26,49 @@ exports.initialize = function(pathsObj) {
 // The following function names are provided to you to suggest how you might
 // modularize your code. Keep it clean!
 
+
 exports.readListOfUrls = function(callback) {
+  fs.readFile(this.paths.list, 'utf8', (err, data) => {
+    callback(data.split('\n'));
+  });
 };
 
 exports.isUrlInList = function(url, callback) {
+  fs.readFile(this.paths.list, 'utf8', (err, data) => {
+    callback(data.split('\n').includes(url));
+  });
 };
 
 exports.addUrlToList = function(url, callback) {
+  fs.appendFile(this.paths.list, url + '\n', callback);
 };
 
 exports.isUrlArchived = function(url, callback) {
+  fs.readFile(this.paths.archivedSites + '/' + url, 'utf8', (err, data) => {
+    err ? callback(false) : callback(true);
+  });
 };
 
 exports.downloadUrls = function(urls) {
+
+  for (var i = 0; i < urls.length; i++) {
+    this.isUrlArchived(urls[i], (fileExists) => {
+      if (!fileExists) {
+        //download
+        http.get(urls[i], (res) => {
+          let rawData = '';
+          res.on('data', (chunk) => { rawData += chunk; });
+          res.on('end', () => {
+            try {
+              // const parsedData = JSON.parse(rawData);
+              console.log('RAWDATA$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$', rawData);
+              //fs.writeFileSync(this.paths.archivedSites + '/' +  urls[i], parsedData);
+            } catch (e){
+              console.error(e.message);
+            }
+          });
+        });
+      }
+    });
+  }
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,38 @@
 var path = require('path');
-var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var archive = require('../helpers/archive-helpers.js');
+var httpHelpers = require('./http-helpers.js');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+var handleRequest = function (req, res) {
+
+  if (req.method === 'GET' && req.url === '/') { //grab the homepage
+    fs.readFile( archive.paths.siteAssets+'/index.html' , (err, data) => {
+      if(err){
+        res.writeHead(404, archive.headers);
+        res.end("404: NOT FOUND");
+      }
+      httpHelpers.serveAssets(res, data);
+    });
+  } else if (req.method === 'GET') {
+    console.log(archive.paths.archivedSites + req.url);
+    fs.readFile( archive.paths.archivedSites + req.url , (err, data) => {
+      if(err){
+        res.writeHead(404, archive.headers);
+        res.end("404: NOT FOUND");
+      }
+      httpHelpers.serveAssets(res, data);
+    });
+  } else if (req.method === 'POST' ) {
+    var fixturePath = archive.paths.list;
+    req.on('data', (data) => {
+      fs.writeFileSync(fixturePath, data.toString().split('=')[1] + '\n');
+      res.writeHead(302, archive.headers);
+      res.end("302: FOUND");
+    });
+
+  }
+
 };
+
+module.exports.handleRequest = handleRequest;
